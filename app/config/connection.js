61c6f4f004a1902750ce83eff5fcdf8c00addbfa
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-06-29 19:25:23 -0400

    Added mMelee and Ranged

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,7 +1,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("pfdb", "root", "root", {
+var sequelize = new Sequelize("pfdb", "root", "rootroot", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
