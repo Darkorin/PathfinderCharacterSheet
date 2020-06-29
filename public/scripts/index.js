4fc5655b164ba6f4bf79955a4ff151ccda63f9b9
David Stewart <darkorin00@hotmail.com>
2020-06-28 23:16:11 -0400

    script updates, basic character template, part of the overview page

---
+++ public/scripts/index.js
@@ -0,0 +1,46 @@
+//  ----------CAROUSELS -----------------
+
+$(document).ready(function () {
+    $("#myCarousel").on("slide.bs.carousel", function (e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 3;
+        var totalItems = $(".carousel-item").length;
+
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i = 0; i < it; i++) {
+                // append slides to end
+                if (e.direction == "left") {
+                    $(".carousel-item")
+                        .eq(i)
+                        .appendTo(".carousel-inner");
+                } else {
+                    $(".carousel-item")
+                        .eq(0)
+                        .appendTo($(this).find(".carousel-inner"));
+                }
+            }
+        }
+    });
+});
+
+const rangeSlider = function () {
+    const slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+
+    slider.each(function () {
+
+        value.each(function () {
+            const value = $(this).prev().attr('value');
+            $(this).html(value);
+        });
+
+        range.on('input', function () {
+            $(this).next(value).html(this.value);
+        });
+    });
+};
+
+rangeSlider();
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Dependencies
 const fs = require('fs');
 const express = require('express');
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 
 // Create an instance of the express app.
 const app = express();
@@ -15,7 +16,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static('public'))
 
-// Creates an empty db object to import ot from our json file
+// Handlebars Helper for calculating stat mods
+Handlebars.registerHelper("calcMod", function (baseScore, tempMod) {
+    return -Math.ceil((10 - (baseScore + tempMod)) / 2);
+});
+
+// Creates an empty db object to import to from our json file
 const db = {
     feats: [],
     racialTraits: [],
@@ -29,41 +35,83 @@ const db = {
 const importDB = () => {
     fs.readFile("./data/pfdb.json", "utf8", (err, content) => {
         if (err) throw err;
-        content = JSON.parse(content);   
-        const index = ["feats", "racialTraits", "races", "skills", "classes", "spells"];     
-        for(let i = 0; i < content.length; i++){
-            db[index[i]] =  content[i];
+        content = JSON.parse(content);
+        const index = ["feats", "racialTraits", "races", "skills", "classes", "spells"];
+        for (let i = 0; i < content.length; i++) {
+            db[index[i]] = content[i];
         }
     });
 }
 
 // Character Initialization Page Route
-app.get("/create", (req, res) =>{
-    res.render("index", {index: true, races: db.races, classes: db.classes});
-} )
+app.get("/create", (req, res) => {
+    res.render("index", { index: true, races: db.races, classes: db.classes });
+})
 
 //
 // Character editor For testing only
-app.get("/stats", (req, res) =>{
-    res.render("stats", {index: false,race: db.races[0], classes: db.classes[0], feats: db.feats, skills: db.skills, spells: db.spells.filter(spell => {
-        let spellUsable = false;
-        spell.levels.forEach(pfclass => {
-             if (pfclass.class === db.classes[0].name) spellUsable = true;
-        });
-        return spellUsable;
-    })});
-} )
+app.get("/stats", (req, res) => {
+    
+    let racePlural = db.races[0].name;
+    let raceTraitsURL = db.racialTraits[0][0].url;
+    let index = raceTraitsURL.search(racePlural);
+    let race = raceTraitsURL.substring(index+racePlural.length+1, raceTraitsURL.search(' Racial') +1);
+
+    res.render("stats", {
+        //Page
+        index: false,
+        //DB export
+        race: db.races[0],
+        class: db.classes[0],
+        feats: db.feats,
+        skills: db.skills,
+        spells: db.spells.filter(spell => {
+            let spellUsable = false;
+            spell.levels.forEach(pfclass => {
+                if (pfclass.class === db.classes[0].name) spellUsable = true;
+            });
+            return spellUsable;
+        }),
+
+        //Character export
+        descriptive: {
+        name: "Darkorin",
+        alignment: "CN",
+        height: "5'8",
+        weight: "150lb",
+        hair: "black",
+        eyes: "blue"
+        },
+        level: 1,
+        raceName: race,
+        scores: {
+            str: { score: "STR", value: 18, temp: 2 },
+            dex: { score: "DEX", value: 7, temp: 1 },
+            con: { score: "CON", value: 5, temp: 2 },
+            int: { score: "INT", value: 9, temp: 3 },
+            wis: { score: "WIS", value: 14, temp: 1 },
+            cha: { score: "CHA", value: 8, temp: -4 }
+        },
+        hp: 30,
+        money: {
+            c: 70,
+            s: 51,
+            g: 31,
+            p: 10
+        }
+    });
+})
 
 // Character editor
-app.get("/editor/:charId?", (req, res) =>{
-    res.render("stats", {race: db.races[0], classes: db.classes[0], feats: db.feats, skills: db.skills, spells: db.spells});
-} )
+app.get("/editor/:charId?", (req, res) => {
+    res.render("stats", { race: db.races[0], classes: db.classes[0], feats: db.feats, skills: db.skills, spells: db.spells });
+})
 
 // Imports the DB
 importDB();
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
