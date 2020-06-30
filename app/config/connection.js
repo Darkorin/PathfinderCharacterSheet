9d5a1a35d33d1ce6173d52bdc994f946ac1afa8a
David Stewart <darkorin00@hotmail.com>
2020-06-30 18:03:16 -0400

    possible heroku fix

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,8 +1,10 @@
 var Sequelize = require("sequelize");
-var config = require(__dirname + '/config.json');
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + '/config.json')[env];
+
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-if (config.production.use_env_variable) {
+if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config.development)
