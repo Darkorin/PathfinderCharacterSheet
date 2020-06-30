548d972b3668f0602579382f791b583be68aa196
David Stewart <darkorin00@hotmail.com>
2020-06-30 12:54:48 -0400

    more heroku debugging

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,12 +1,12 @@
 var Sequelize = require("sequelize");
-var config = require(__dirname + '\\config.json');
+var config = require(__dirname + '/config.json');
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config.development)
-// }
+}
 
 // Exports the connection for other files to use
 module.exports = sequelize;
