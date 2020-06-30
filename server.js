285c6cc294463b5bef8d4ad186961ba75e2ba07e
David Stewart <darkorin00@hotmail.com>
2020-06-30 16:02:06 -0400

    More google testing

--- server.js
+++ server.js
@@ -172,23 +172,8 @@ app.post("/api/:charId/:query", (req, res) => {
 })
 
 app.post("/login", (req, res) => {
-    console.log(req.body);
-    const { OAuth2Client } = require('google-auth-library');
-    const client = new OAuth2Client(1029747393830 - kfqdq1b5d6tsuf1eo0m3jirl6ppeps6o.apps.googleusercontent.com);
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: req.body,
-            audience: 1029747393830 - kfqdq1b5d6tsuf1eo0m3jirl6ppeps6o.apps.googleusercontent.com,  // Specify the CLIENT_ID of the app that accesses the backend
-            // Or, if multiple clients access the backend:
-            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-        });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
-        console.log(userid + "has logged in!")
-        // If request specified a G Suite domain:
-        // const domain = payload['hd'];
-    }
-    verify().catch(console.error);
+ console.log(req.body);
+
 })
 
 // Creates a dummy character for testing
